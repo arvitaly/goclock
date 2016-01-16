36f632db238003ea93c2dd5e5a64dca472217aa8
arvitaly <arvitaly@list.ru>
2016-01-16 15:22:01 +0300

    Add mocks by gomock and method Force for moving time

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# goclock
-Adapter for package "time" by interface
+Goclock
+Adapter for package "time" by interface with mocks
+
+Example:
+
+func TestAfter(t *testing.T) {
+
+	var ctrl = gomock.NewController(t)
+	var clock = NewMockClock(ctrl)
+
+	//After should be completed
+	var done = clock.After(time.Second * 1000)
+	//Shift a time
+	clock.Force(time.Second * 1100)
+	select {
+	case <-time.After(time.Second):
+		t.Error("Timeout")
+	case <-done:
+
+	}
+
+	//After should not be completed
+	done = clock.After(time.Second * 1000)
+	clock.Force(time.Second * 900)
+	select {
+	case <-time.After(time.Second):
+
+	case <-done:
+		t.Error("After don't be complete")
+	}
+
+}
---
+++ mock_goclock/mock_goclock_test.go
@@ -0,0 +1,45 @@
+package mock_goclock
+
+import "testing"
+import "github.com/golang/mock/gomock"
+import "time"
+
+func TestSleep(t *testing.T) {
+	var ctrl = gomock.NewController(t)
+	var clock = NewMockClock(ctrl)
+	var done = make(chan bool)
+	go func() {
+		done <- true
+		clock.Sleep(time.Second * 10)
+		done <- true
+	}()
+	<-done
+	clock.Force(time.Second * 11)
+	select {
+	case <-time.After(time.Second):
+		t.Error("Timeout")
+	case <-done:
+
+	}
+}
+func TestAfter(t *testing.T) {
+	var ctrl = gomock.NewController(t)
+	var clock = NewMockClock(ctrl)
+	var done = clock.After(time.Second * 1000)
+	clock.Force(time.Second * 1100)
+	select {
+	case <-time.After(time.Second):
+		t.Error("Timeout")
+	case <-done:
+
+	}
+	//
+	done = clock.After(time.Second * 1000)
+	clock.Force(time.Second * 900)
+	select {
+	case <-time.After(time.Second):
+
+	case <-done:
+		t.Error("After don't be complete")
+	}
+}
